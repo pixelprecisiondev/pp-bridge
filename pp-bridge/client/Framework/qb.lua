032082b1fa10708d18c00296a638da5895db8d2f
PixelPrecision <59230412+pixelprecisiondev@users.noreply.github.com>
2024-05-28 22:07:39 +0200

    Fixed an error with missing data without a selected character

--- pp-bridge/client/Framework/qb.lua
+++ pp-bridge/client/Framework/qb.lua
@@ -2,42 +2,49 @@ local data = {}
 
 QBCore = exports['qb-core']:GetCoreObject()
 
-local function loadPlayerData()
+
+
+data.getName = function ()
     local charinfo = QBCore.Functions.GetPlayerData().charinfo
-
-    data.getName = function ()
+    if not charinfo then 
         return {
-            fullName = charinfo.firstname .. ' ' .. charinfo.lastname,
-            firstName = charinfo.firstname,
-            lastName = charinfo.lastname
-        }
+            fullName = '',
+            firstName = '',
+            lastName = ''
+        } 
     end
-
-    data.getJob = function()
-        local job = QBCore.Functions.GetPlayerData().job
-        return {
-            name = job.name,
-            label = job.label,
-            grade = job.grade.level,
-            grade_label = job.grade.name
-        }
-    end
-
-    data.TriggerServerCallback = function(name, cb, ...)
-        QBCore.Functions.TriggerCallback(name, cb, ...)
-    end
-
-    data.getSex = function()
-        return QBCore.Functions.GetPlayerData().charinfo.gender == 1 and "male" or "female"
-    end
+    return {
+        fullName = charinfo.firstname .. ' ' .. charinfo.lastname,
+        firstName = charinfo.firstname,
+        lastName = charinfo.lastname
+    }
 end
 
-RegisterNetEvent('QBCore:Client:OnPlayerLoaded', function()
-    loadPlayerData()
-end)
+data.getJob = function()
+    local job = QBCore.Functions.GetPlayerData().job
+    if not job then 
+        return {
+            name = '',
+            label = '',
+            grade = 0,
+            grade_label = ''
+        }
+    end
+    return {
+        name = job.name,
+        label = job.label,
+        grade = job.grade.level,
+        grade_label = job.grade.name
+    }
+end
 
-if QBCore.Functions.GetPlayerData().charinfo ~= nil then
-    loadPlayerData()
+data.TriggerServerCallback = function(name, cb, ...)
+    QBCore.Functions.TriggerCallback(name, cb, ...)
+end
+
+data.getSex = function()
+    local charinfo = QBCore.Functions.GetPlayerData().charinfo
+    return charinfo and (charinfo.gender == 1 and "male" or "female") or "male"
 end
 
 return data
